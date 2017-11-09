f4b59f34e7339b2e27bd0d11ea7affed1822e24b
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-11-08 22:37:15 -0800

    #001 - Intial push of project to github

---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour 
+{
+	public static LevelManager instance;
+	float TIME_TILL_GAME_SCENE = 2f;
+
+	void Awake()
+	{
+		if (!instance)
+		{
+			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+		
+	void Update()
+	{
+		if (Time.time >= TIME_TILL_GAME_SCENE && SceneManager.GetActiveScene().buildIndex == 0)
+		{
+			LoadLevel("Game");
+		}
+	}
+
+	public void LoadLevel(string levelName)
+	{
+		SceneManager.LoadScene(levelName);
+	}
+}
---
+++ Assets/_Scripts/PlayerMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour 
+{
+	[SerializeField]
+	float m_PlayerSpeed;
+
+	void FixedUpdate () 
+	{
+		//Move Player with WASD / UDLR
+		transform.Translate(Input.GetAxis("Horizontal") * m_PlayerSpeed, Input.GetAxis("Vertical") * m_PlayerSpeed, 0f);
+	}
+}
