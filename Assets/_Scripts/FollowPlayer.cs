beb8fbcf31189f25a0fb0991bede273f499d7504
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-11-09 22:20:16 -0800

    #004 - GameController script added.  Camera follows player script made

---
+++ Assets/_Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour 
+{
+	Vector3 m_Offset;
+
+	void Start()
+	{
+		m_Offset = transform.position - GameController.instance.ReturnPlayerPos();
+	}
+
+	void Update()
+	{
+		transform.position = GameController.instance.ReturnPlayerPos() + m_Offset;
+	}
+}
---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour 
+{
+	[SerializeField]
+	GameObject m_Player;
+
+	public static GameController instance;
+
+	void Awake()
+	{
+		if (!instance)
+		{
+			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	public Vector3 ReturnPlayerPos()
+	{
+		return new Vector3(m_Player.transform.localPosition.x, m_Player.transform.localPosition.y, 0f);
+	}
+}
