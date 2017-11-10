f4ec7db06f3c937f5f487329e359185c8a775cd7
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-11-09 22:45:08 -0800

    #005 - Created Desert terrain, estates on player touch to determine what terrain states player is on

--- Assets/_Scripts/PlayerTouch.cs
+++ Assets/_Scripts/PlayerTouch.cs
@@ -4,11 +4,45 @@ using UnityEngine;
 
 public class PlayerTouch : MonoBehaviour 
 {
+	public enum eTerrain
+	{
+		grass, mountain, snow, desert
+	};
+	private eTerrain eState = eTerrain.grass;
+
+	void Update()
+	{
+		eTerrain currentState = eState;
+
+		switch(currentState)
+		{
+			case eTerrain.grass:
+				{
+					Debug.Log("You are on grass.");
+				}
+				break;
+			case eTerrain.desert:
+				{
+					Debug.Log("You are on the dessert");
+				}
+				break;
+			default:
+				{
+					Debug.Log("You are not stepping on anything");
+					break;
+				}
+		}
+	}
+
 	void OnTriggerEnter2D (Collider2D myCol)
 	{
 		if (myCol.gameObject.tag == "Home")
 		{
 			Debug.Log("You've reached your home.");
+		}
+		else if (myCol.gameObject.tag == "Desert")
+		{
+			eState = eTerrain.desert;
 		}
 	}
 }
