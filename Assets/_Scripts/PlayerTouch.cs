ad5469262ac96f70e66bec2215cdb3a2f965088f
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-11-09 22:32:34 -0800

    #005 - Made a tent - Home with collider detection

---
+++ Assets/_Scripts/PlayerTouch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTouch : MonoBehaviour 
+{
+	void OnTriggerEnter2D (Collider2D myCol)
+	{
+		if (myCol.gameObject.tag == "Home")
+		{
+			Debug.Log("You've reached your home.");
+		}
+	}
+}
