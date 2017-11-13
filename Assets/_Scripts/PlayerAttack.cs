8b56b13802bbf3a52f0f80371d21040d6ec34bcb
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-11-12 23:07:42 -0800

    #009 - New player sprite, slashing sprite, press spacebar to slash (but no collider)

---
+++ Assets/_Scripts/PlayerAttack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour 
+{
+	[SerializeField]
+	SpriteRenderer m_Attack;
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Attack"))
+		{
+			m_Attack.enabled = true;
+		}
+		else
+		{
+			m_Attack.enabled = false;
+		}
+	}
+
+}
