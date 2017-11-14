b656e2ae40c3faa4ff4711313b529bc2e3910e1c
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-11-13 20:55:51 -0800

    #010 - Worm enemy created, moves towards player every few seconds, dies when hit by player

---
+++ Assets/_Scripts/EnemyMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour 
+{
+	[SerializeField]
+	float m_EnemyMovement;
+	float m_Timer = 0f;
+	float MOVE_DURATION = 2f;
+
+
+	void FixedUpdate()
+	{
+		m_Timer += Time.deltaTime;
+		if (m_Timer > MOVE_DURATION)
+		{
+			MoveTowardsPlayer();
+		}
+	}
+
+	void MoveTowardsPlayer()
+	{
+		//transform.localPosition;
+		//Always aim to make current pos = GameController.instance.GetPlayerPos
+		// + / - x pos and + / - y pos
+		Vector2 currentPos = new Vector2(transform.localPosition.x, transform.localPosition.y);
+		Vector2 playerPos = GameController.instance.GetPlayerPos();
+
+		if (currentPos.x < playerPos.x)
+		{
+			Vector2 newPos = transform.localPosition;
+			newPos.x += m_EnemyMovement;
+			transform.localPosition = newPos;
+		}
+		else
+		{
+			Vector2 newPos = transform.localPosition;
+			newPos.x -= m_EnemyMovement;
+			transform.localPosition = newPos;
+		}
+
+		if (currentPos.y < playerPos.y)
+		{
+			Vector2 newPos = transform.localPosition;
+			newPos.y += m_EnemyMovement;
+			transform.localPosition = newPos;
+		}
+		else
+		{
+			Vector2 newPos = transform.localPosition;
+			newPos.y -= m_EnemyMovement;
+			transform.localPosition = newPos;
+		}
+
+		if (m_Timer > 4f)
+		{
+			m_Timer = 0f;
+		}
+	}
+}
--- Assets/_Scripts/PlayerAttack.cs
+++ Assets/_Scripts/PlayerAttack.cs
@@ -6,16 +6,28 @@ public class PlayerAttack : MonoBehaviour
 {
 	[SerializeField]
 	SpriteRenderer m_Attack;
+	[SerializeField]
+	BoxCollider2D m_AttackCollider;
 
 	void Update()
 	{
 		if (Input.GetButtonDown("Attack"))
 		{
 			m_Attack.enabled = true;
+			m_AttackCollider.enabled = true;
 		}
 		else
 		{
 			m_Attack.enabled = false;
+			m_AttackCollider.enabled = false;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D myCol)
+	{
+		if (myCol.gameObject.tag == "Worm")
+		{
+			Destroy(myCol.gameObject);
 		}
 	}
 
