a0df7cb7e96c46713b942d58647ec464b076b293
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-11-23 22:39:06 -0800

    #030 - Enemies fade into game

---
+++ Assets/FadeIn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour 
+{
+	float FADE_DURATION = 1f;
+	Timer m_FadeTimer = new Timer();
+	SpriteRenderer m_SpriteRender;
+
+	void Start()
+	{
+		m_SpriteRender = this.gameObject.GetComponent<SpriteRenderer>();
+		m_FadeTimer.Set(FADE_DURATION);
+	}
+
+	void Update()
+	{
+		m_FadeTimer.Update(Time.deltaTime);
+		if (!m_FadeTimer.HasCompleted())
+		{
+			Color currentColor = m_SpriteRender.color;
+			currentColor.a = (FADE_DURATION - m_FadeTimer.GetTime()) / FADE_DURATION;
+			m_SpriteRender.color = currentColor;
+		}
+	}
+
+}
