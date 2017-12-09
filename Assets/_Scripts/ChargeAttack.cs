8ec8944eb90bfd33c8a2c3e441206cba135641a5
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-12-09 12:20:56 -0800

    #051 - Working on walking animation for player

--- Assets/_Scripts/ChargeAttack.cs
+++ Assets/_Scripts/ChargeAttack.cs
@@ -15,15 +15,27 @@ public class ChargeAttack : MonoBehaviour
 	int MIN_ATTEMPTS = 1;
 	int MAX_ATTEMPTS = 3;
 
+	[SerializeField]
+	float m_Speed;
+
+	Vector2 m_Direction;
+
 	void Update()
 	{
 		if (m_ChargeAttempts > 0 && m_ChargeTimer.Update(Time.deltaTime))
 		{
 			//Charge at player
-			Debug.Log("Can Charge");
 			m_ChargeAttempts--;
-			Debug.Log("Charge Attempts = " + m_ChargeAttempts);
 			m_ChargeTimer.Set(m_ChargeDuration);
+			Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
+			Vector2 endPos = new Vector2(GameController.instance.ReturnPlayerPos().x, GameController.instance.ReturnPlayerPos().y);
+			SetProjectileDirection(currentPos, endPos);
+			m_MoveTimer.Set(m_MoveDuration);
+		}
+
+		if (!m_MoveTimer.Update(Time.deltaTime))
+		{
+			transform.Translate(m_Direction * m_Speed, Space.World);
 		}
 	}
 
@@ -31,5 +43,10 @@ public class ChargeAttack : MonoBehaviour
 	{
 		m_ChargeTimer.Set(m_ChargeDuration);
 		m_ChargeAttempts = Random.Range(MIN_ATTEMPTS, MAX_ATTEMPTS);
+	}
+
+	public void SetProjectileDirection(Vector2 currentPos, Vector2 endPos)
+	{
+		m_Direction = (endPos - currentPos).normalized;
 	}
 }
