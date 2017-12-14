c24197c05a5b05793414ef1c02eba7eef93d484e
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-12-13 22:16:19 -0800

    #059 - Fixed animation again, selfdestruct script on fireballs

---
+++ Assets/_Scripts/SelfDestruct.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour 
+{
+	[SerializeField]
+	float WAIT_DURATION;
+
+	void Start()
+	{
+		Destroy(gameObject, WAIT_DURATION);
+	}
+}
