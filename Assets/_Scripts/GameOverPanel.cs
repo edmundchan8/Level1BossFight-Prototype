2638aae82cdb46c807949df2f3c8b30a7e3184f8
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-12-23 15:51:01 -0800

    #073 - Gave boss cIrclecollider2D (not sure why enemies use circle collider).  Win screen shows when boss defeated.  Duration of game over and win is different (win is longer).  Need to implement boss death animation, stop boss movement, stop player movement, destroy boss asap on boss death.

--- Assets/_Scripts/GameOverPanel.cs
+++ Assets/_Scripts/GameOverPanel.cs
@@ -8,22 +8,28 @@ public class GameOverPanel : MonoBehaviour
 	Image m_Panel;
 	Timer m_FadeTimer = new Timer();
 	[SerializeField]
-	float FADE_DURATION;
+	float GAMEOVER_FADE_DURATION;
+	[SerializeField]
+	float WIN_FADE_DURATION;
 	[SerializeField]
 	Button m_GameOver;
+	[SerializeField]
+	Button m_Win;
 
 	void Start()
 	{
 		m_Panel = GetComponent<Image>();
 		StartCoroutine("SetDeadTimer");
+		StartCoroutine("SetWinTimer");
 		m_GameOver.gameObject.SetActive(false);
+		m_Win.gameObject.SetActive(false);
 	}
 
 	void Update()
 	{
 		if (GameController.instance.ReturnPlayerStats().PlayerDead())
 		{
-			float alpha = (FADE_DURATION - m_FadeTimer.GetTime()) / FADE_DURATION;
+			float alpha = (GAMEOVER_FADE_DURATION - m_FadeTimer.GetTime()) / GAMEOVER_FADE_DURATION;
 			if (!m_FadeTimer.Update(Time.deltaTime))
 			{
 				Color currentColor = m_Panel.color;
@@ -33,6 +39,20 @@ public class GameOverPanel : MonoBehaviour
 			else if (m_FadeTimer.Update(Time.deltaTime) && m_FadeTimer.HasCompleted())
 			{
 				m_GameOver.gameObject.SetActive(true);
+			}
+		}
+		else if (GameController.instance.ReturnBossStats().IsEnemyDead())
+		{
+			float alpha = (WIN_FADE_DURATION - m_FadeTimer.GetTime()) / WIN_FADE_DURATION;
+			if (!m_FadeTimer.Update(Time.deltaTime))
+			{
+				Color currentColor = m_Panel.color;
+				currentColor.a = alpha;
+				m_Panel.color = currentColor;
+			}
+			else if (m_FadeTimer.Update(Time.deltaTime) && m_FadeTimer.HasCompleted())
+			{
+				m_Win.gameObject.SetActive(true);
 			}
 		}
 	}
@@ -49,7 +69,14 @@ public class GameOverPanel : MonoBehaviour
 	IEnumerator SetDeadTimer()
 	{
 		yield return new WaitUntil(() => GameController.instance.ReturnPlayerStats().PlayerDead());
-		m_FadeTimer.Set(FADE_DURATION);
+		m_FadeTimer.Set(GAMEOVER_FADE_DURATION);
 		SetPanelColor(Color.black);
+	}
+
+	IEnumerator SetWinTimer()
+	{
+		yield return new WaitUntil(() => GameController.instance.ReturnBossStats().IsEnemyDead());
+		m_FadeTimer.Set(WIN_FADE_DURATION);
+		SetPanelColor(Color.white);
 	}
 }
